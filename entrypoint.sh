99238fbb82ec7114adc9e571da6c3a1fae8e66b6
Bryan Cross <bryancross@github.com>
2019-07-16 14:40:54 -0500

    added gpr code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,8 @@
 
 
 echo hello
+git config user.name "GPR User"
+git config user.email "noreply@github.com"
+npm version 1.0.$(date +%s)
+npm config set //npm.pkg.github.com/:_authToken ${{GPR_TOKEN}}
+npm publish
