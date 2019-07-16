2d11206b0257b20d2d27a88aa62fa844213dcd4d
Bryan Cross <bryancross@github.com>
2019-07-16 14:23:35 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,1 @@
-echo Hello world
-#git config user.name "GPR User"
-#git config user.email "noreply@github.com"
-#npm version 1.0.$(date +%s)
-#npm config set //npm.pkg.github.com/:_authToken ${{GPR_TOKEN}}
-#npm publish
-
+echo Hello
