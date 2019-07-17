baff7116ac7087fb6feedcf0b32ceb370b1cd552
Bryan Cross <bryancross@github.com>
2019-07-16 22:39:31 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ git config user.email "noreply@github.com"
 printf "hello again\n"
 npm version 1.0.$(date +%s)
 npm config set //npm.pkg.github.com/:_authToken ${GPR_TOKEN}
-#npm publish
+npm publish
 
