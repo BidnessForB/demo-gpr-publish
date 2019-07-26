26ed0a74ebd3c348f0a46e8518d546c3545a501f
Bryan Cross <bryancross@github.com>
2019-07-25 23:17:28 -0700

    Quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e -u -o pipefail
 
 printf "Actor: " 
 printf "${GITHUB_ACTOR}"
-git config user.name ${GITHUB_ACTOR}
+git config user.name "${GITHUB_ACTOR}"
 git config user.email "noreply@github.com"
 printf "Publishing to GPR...\n"
 npm version 1.0.$(date +%s)
