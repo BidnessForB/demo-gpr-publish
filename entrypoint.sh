afdd6d3778ace768180169fc56361a536db1d603
Bryan Cross <bryancross@github.com>
2019-07-25 22:44:42 -0700

    Removing git commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e -u -o pipefail
 
-git config user.name "GPR User"
-git config user.email "noreply@github.com"
-printf "hello again\n"
+#git config user.name "GPR User"
+#git config user.email "noreply@github.com"
+printf "Publishing to GPR...\n"
 npm version 1.0.$(date +%s)
 npm config set //npm.pkg.github.com/:_authToken ${GPR_TOKEN}
 npm publish
