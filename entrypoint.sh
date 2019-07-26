e3c5fc098c0430e23762ca0bd8e821ee3874106f
Bryan Cross <bryancross@github.com>
2019-07-26 08:47:41 -0700

    Env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 set -e -u -o pipefail
 
-printf "Actor: " 
-printf "${GITHUB_ACTOR}"
-git config user.name "${GITHUB_ACTOR}"
+printf "${GITHUB_ACTOR}\n"
+git config user.name "GPR User"
 git config user.email "noreply@github.com"
 printf "Publishing to GPR...\n"
 npm version 1.0.$(date +%s)
