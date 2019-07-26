230c738d55738f339ead74634573eca40e93fc3d
Bryan Cross <bryancross@github.com>
2019-07-25 23:13:14 -0700

    Actor var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e -u -o pipefail
 
+printf "Actor: " 
+printf "${GITHUB_ACTOR}"
 git config user.name ${GITHUB_ACTOR}
 git config user.email "noreply@github.com"
 printf "Publishing to GPR...\n"
