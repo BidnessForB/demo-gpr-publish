1cd8c490dae6d273839d0c52f2095bf650c45d0a
Bryan Cross <bryancross@github.com>
2019-07-25 22:52:58 -0700

    Adding var for actor

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e -u -o pipefail
 
-#git config user.name "GPR User"
-#git config user.email "noreply@github.com"
+git config user.name ${GITHUB_ACTOR}
+git config user.email "noreply@github.com"
 printf "Publishing to GPR...\n"
 npm version 1.0.$(date +%s)
 npm config set //npm.pkg.github.com/:_authToken ${GPR_TOKEN}
